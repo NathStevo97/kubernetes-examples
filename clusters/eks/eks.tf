c058e14bff577c82ff6c1f3c428ecd199931cff8
NathStevo97 <natstephenson15@gmail.com>
2026-03-27 23:14:55 +0000

    fix: eks addons

--- clusters/eks/eks.tf
+++ clusters/eks/eks.tf
@@ -5,31 +5,36 @@ module "eks" {
   name               = local.eks_cluster_name
   kubernetes_version = var.eks_version
 
-  vpc_id                 = module.vpc.vpc_id
-  subnet_ids             = module.vpc.private_subnets
-  endpoint_public_access = true
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  # EKS Addons
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
 
   # https://docs.aws.amazon.com/eks/latest/APIReference/API_Nodegroup.html#AmazonEKS-Type-Nodegroup-amiType
 
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+    example = {
+      name = "example-group"
 
       instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 3
       desired_size = 2
-    }
 
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      ami_type = "AL2023_x86_64_STANDARD"
     }
   }
 }
--- clusters/eks/variables.tf
+++ clusters/eks/variables.tf
@@ -1,8 +1,11 @@
 variable "eks_version" {
-  default = "1.33"
+  default     = "1.34"
+  type        = string
+  description = "Kubernetes version for EKS cluster deployment"
 }
 
 variable "region" {
   description = "AWS Region"
   default     = "eu-west-2"
+  type        = string
 }
